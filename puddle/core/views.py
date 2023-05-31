d7dec311208fbd1ac3f80ead3f5963fb707399d9
Tatiana Johnson <Tatianarjo@gmail.com>
2023-05-31 16:10:18 -0500

    added nav bar and footer

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Creates pages
 
 def index(request):
     return render(request, 'core/index.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
